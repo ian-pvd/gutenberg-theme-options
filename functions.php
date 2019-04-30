b3edb68f713de52fc3634e2f3eacacb8d4961b7f
Ian Del Giudice <ian.delgiudice@10up.com>
2019-04-30 12:08:39 -0400

    Setup: add theme hooks file for gutenberg options

--- functions.php
+++ functions.php
@@ -159,3 +159,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Theme Hooks
+ */
+require get_template_directory() . '/inc/theme-hooks.php';
---
+++ inc/theme-hooks.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Theme Hooks
+ *
+ * Functions which enhance the theme by hooking into WordPress
+ *
+ * @package uve-got-options
+ */
