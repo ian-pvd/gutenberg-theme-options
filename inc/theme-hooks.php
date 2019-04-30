6e7032ca6ccd1510f6f512d8ffaaf424db86e419
Ian Del Giudice <ian.delgiudice@10up.com>
2019-04-30 12:12:15 -0400

    Add theme color palette options and styles, disable custom colors

--- inc/theme-hooks.php
+++ inc/theme-hooks.php
@@ -19,5 +19,35 @@ function gutenberg_theme_support_options() {
 	// Enqueue editor styles.
 	add_editor_style( 'post-content.css' );
 
+	// Add editor color palette.
+	add_theme_support(
+		'editor-color-palette',
+		[
+			[
+				'name'  => __( 'Red', 'uve-got-options' ),
+				'slug'  => 'red',
+				'color' => '#E12627',
+			],
+			[
+				'name'  => __( 'Dark Gray', 'uve-got-options' ),
+				'slug'  => 'gray-dark',
+				'color' => '#414242',
+			],
+			[
+				'name'  => __( 'Blue', 'uve-got-options' ),
+				'slug'  => 'blue',
+				'color' => '#3EC1CC',
+			],
+			[
+				'name'  => __( 'Green', 'uve-got-options' ),
+				'slug'  => 'green',
+				'color' => '#99D04A',
+			],
+		]
+	);
+
+	// Disable custom theme colors.
+	add_theme_support( 'disable-custom-colors' );
+
 }
 add_action( 'after_setup_theme', 'gutenberg_theme_support_options' );
